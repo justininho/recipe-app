d6a4d08fba7a78a0ec813a6f817f9b9d4495cb27
Justin <github@jlri.net>
2026-01-21 16:25:50 -0500

    - adding nativewind and react-native-reusables - updating .gitignore

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,8 +2,10 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
+import "../global.css"
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import {PortalHost} from "@rn-primitives/portal";
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -19,6 +21,7 @@ export default function RootLayout() {
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
       <StatusBar style="auto" />
+      <PortalHost />
     </ThemeProvider>
   );
 }
