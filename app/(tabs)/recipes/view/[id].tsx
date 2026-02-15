f57f808003690f28a9aa959c13ece05ec5a9ccbe
Justin <github@jlri.net>
2026-02-15 11:14:27 -0500

    updating recipe view / mocks

--- app/(tabs)/recipes/view/[id].tsx
+++ app/(tabs)/recipes/view/[id].tsx
@@ -2,55 +2,98 @@ import {Link, Stack, useLocalSearchParams} from 'expo-router';
 import {MOCK_RECIPES} from '@/features/recipes/mocks/recipe.mocks';
 import {Button} from '@/components/ui/button';
 import {Text} from '@/components/ui/text';
-import {View} from 'react-native';
+import {ScrollView, View} from 'react-native';
+import {Image} from "expo-image";
+import React from "react";
+import {ChefHat, Clock, MoveRight, SquarePen} from "lucide-react-native";
+import {Icon} from "@/components/ui/icon";
+import {Badge} from "@/components/ui/badge";
 
 export default function RecipeScreen() {
   const {id} = useLocalSearchParams<{ id: string }>();
   const recipe = MOCK_RECIPES.find(r => r.id === id);
-
+  
   return (
     <>
       <Stack.Screen
         options={{
           headerShown: true,
-          title: recipe?.name ?? 'Recipe'
+          title: recipe ? recipe?.name : 'Recipe Details',
+          headerTitle: () => <></>,
+          headerRight: () => <>
+            <View className="px-6">
+              <Link href={`/recipes/edit/${id}`} asChild>
+                <Button variant="outline" size="icon">
+                  <Icon as={SquarePen}></Icon>
+                </Button>
+              </Link>
+            </View>
+          </>
         }}
       />
-
+      
       {!recipe ? (
         <Text>Recipe not found</Text>
       ) : (
-        <View className="p-4">
-          <Text className="text-2xl font-bold mb-2">{recipe.name}</Text>
-          <Text className="text-sm text-muted-foreground mb-4">{recipe.description}</Text>
-
-          {/* Ingredients */}
-          <View className="mb-4">
-            <Text className="text-lg font-semibold mb-1">Ingredients:</Text>
-            {recipe.ingredients.map((ingredient, index) => (
-              <Text key={index} className="text-sm text-muted-foreground">
-                - {ingredient}
-              </Text>
-            ))}
+        <ScrollView contentInsetAdjustmentBehavior="automatic">
+          
+          <View className="flex">
+            <Image
+              className="flex w-100"
+              style={{height: 250}}
+              source={recipe?.imageUrl}
+              contentFit="cover"
+            />
           </View>
-
-          {/* Instructions */}
-          <View>
-            <Text className="text-lg font-semibold mb-1">Instructions:</Text>
-            {recipe.instructions.map((step, index) => (
-              <Text key={index} className="text-sm text-muted-foreground">
-                {index + 1}. {step}
-              </Text>
-            ))}
-          </View>
-          <View>
-            <Link href={`/recipes/edit/${id}`} asChild>
-              <Button>
-                <Text>Edit Recipe</Text>
+          
+          <View className="p-4">
+            <View className="mb-2">
+              <Text className="text-2xl font-bold">{recipe.name}</Text>
+              <Text className="text-sm text-muted-foreground">{recipe.description}</Text>
+              <View className="flex-row gap-2 mt-3">
+                <Badge variant="secondary" className="flex-row items-center gap-1 px-3 py-1">
+                  <Icon as={Clock} size={12}/>
+                  <Text className="text-xs">Prep {recipe.prepTime}m</Text>
+                </Badge>
+                <Badge variant="secondary" className="flex-row items-center gap-1 px-3 py-1">
+                  <Icon as={ChefHat} size={12}/>
+                  <Text className="text-xs">Cook {recipe.cookTime}m</Text>
+                </Badge>
+              </View>
+            </View>
+            
+            
+            {/* Ingredients */}
+            <View className="mb-4">
+              <Text className="text-lg font-semibold mb-1">Ingredients:</Text>
+              {recipe.ingredients.map(({name}, index) => (
+                <Text key={index} className="text-sm text-muted-foreground">
+                  - {name}
+                </Text>
+              ))}
+            </View>
+            
+            {/* Instructions */}
+            <View>
+              <Text className="text-lg font-semibold mb-1">Instructions:</Text>
+              {recipe.instructions.map(({step, description}, index) => (
+                <Text key={index} className="text-sm text-muted-foreground">
+                  {step}. {description}
+                </Text>
+              ))}
+            </View>
+            
+            <View className="mt-6">
+              {/* todo: link*/}
+              {/*<Link href={`/recipes/edit/${id}`} asChild>*/}
+              <Button variant="outline">
+                <Text>Cook Recipe</Text>
+                <Icon as={MoveRight}/>
               </Button>
-            </Link>
+              {/*</Link>*/}
+            </View>
           </View>
-        </View>
+        </ScrollView>
       )}
     </>
   );
